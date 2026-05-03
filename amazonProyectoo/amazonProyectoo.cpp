46660c5c16307c7bd440821b0a5d99f776d28fda
ales ssj <aleszzz17@outlook.com>
2026-05-02 21:05:48 -0500

    menu integrado

--- amazonProyectoo/amazonProyectoo.cpp
+++ amazonProyectoo/amazonProyectoo.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+void menu()
+{
+	cout << "Bienvenido a Amazon" << endl;
+	cout << "1. Iniciar sesion" << endl;
+	cout << "2. Registrarse" << endl;
+	cout << "3. Salir" << endl;
+}
 
 int main()
 {
