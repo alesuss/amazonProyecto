dd9cdd3672b3457211ab78c761fb65752831bb35
FabrizioAM <aguirref307@gmail.com>
2026-05-07 22:24:39 -0500

    f2

--- Amazon Proyecto/Envio.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "Envio.h"
-
-Envio::Envio(string t, string f) : trackingId(t), estadoEnvio("Procesando"), fechaEstimada(f) {}
-
-void Envio::actualizarEstado(string e) {
-    estadoEnvio = e;
-}
--- Amazon Proyecto/Promocion.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "Promocion.h"
-
-Promocion::Promocion(string c, float d) : codigo(c), descuento(d) {}
-
-bool   Promocion::esValida() { return descuento > 0; }
-string Promocion::getCodigo() { return codigo; }
-float  Promocion::getDescuento() { return descuento; }
